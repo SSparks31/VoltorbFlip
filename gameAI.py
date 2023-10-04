20cfae22b8625397d670e6d095979234886adb6b
Gregorio <gregorio.favero@uel.br>
2023-10-04 11:50:06 -0300

    Acho q melhorei mas na dúvida dá pra voltar

--- gameAI.py
+++ gameAI.py
@@ -6,11 +6,25 @@ class Game(game.Game):
             return 0
         pointsLine, voltorbsLine = self.line(i)
         voltorbsFoundLine = sum(_ == 'X' for _ in self._board[i])
+        pointsFoundLine = sum(_ for _ in self._board[i] if _ not in ('X', ' '))
 
         pointsColumn, voltorbsColumn = self.column(j)
         voltorbsFoundColumn = sum(self._board[_][j] == 'X' for _ in range(5))
+        pointsFoundColumn = sum(self._board[_][j] for _ in range(5) if self._board[_][j] not in ('X', ' '))
         
-        return pointsLine * (voltorbsLine == voltorbsFoundLine) + pointsColumn * (voltorbsColumn == voltorbsFoundColumn)
+        possibilitiesLine = set(('X', 1, 2, 3))
+        possibilitiesColumn = set(('X', 1, 2, 3))
+
+        flippedLine = sum(_ != ' ' for _ in self._board[i])
+        flippedColumn = sum(self._board[_][j] != ' ' for _ in range(5))
+
+        if voltorbsLine == voltorbsFoundLine:
+            possibilitiesLine = set(range(1, pointsLine  + flippedLine - pointsFoundLine - 3))
+        if voltorbsColumn == voltorbsFoundColumn:
+            possibilitiesColumn = set(range(1, pointsColumn + flippedColumn - pointsFoundColumn - 3))
+    
+        p = possibilitiesColumn.intersection(possibilitiesLine)
+        return p if 'X' not in p else 0
     
     def heuristics_second(self, i, j): # Regra 2: Linhas sem possibilidade de valor
         if (self._board[i][j] != ' '):
@@ -80,12 +94,17 @@ class Game(game.Game):
             heuristics = []
             for idx_i, _ in enumerate(self._lines):
                 for idx_j, _ in enumerate(self._columns):
-                    heuristics.append(((idx_i, idx_j), self.heuristics_first(idx_i, idx_j)))
-            heuristics.sort(key=lambda h: h[1])
-            while heuristics[-1][1] > 0:
+                    v = self.heuristics_first(idx_i, idx_j)
+                    if v != 0:
+                        heuristics.append(((idx_i, idx_j), v))
+            heuristics.sort(key=lambda h: len(h[1]), reverse=True)
+            if heuristics:
                 did = True
-                (i, j), _ = heuristics.pop()
-                self._board[i][j] = int(input('MOVIMENTO SEGURO - Vire o quadrado na linha {}, coluna {} e informe o valor encontrado: '.format(i+1, j+1)))
+                (i, j), p = heuristics.pop()
+                if len(p) == 1:
+                    self._board[i][j] = p.pop()
+                else:
+                    self._board[i][j] = int(input('MOVIMENTO SEGURO - Vire o quadrado na linha {}, coluna {} e informe o valor encontrado: '.format(i+1, j+1)))
             
             heuristics = []
             for idx_i, _ in enumerate(self._lines):
